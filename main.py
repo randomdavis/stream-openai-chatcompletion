38f8af28cfd0428d5328c2a752130201ae155e78
Random Davis <random.davis@gmail.com>
2023-05-02 20:24:41 -0700

    update main.py

--- main.py
+++ main.py
@@ -6,34 +6,44 @@ with open('apikey.txt', 'r') as f:
 openai.api_key = API_KEY
 
 
-def stream_chat_completion(prompt, model='gpt-3.5-turbo', temperature=0.8):
+def stream_chat_completion(prompt: str, model: str = 'gpt-3.5-turbo', temperature=0.8,
+                           system_prompt: str = "You are a helpful AI assistant.", conversation_history=None):
+    if conversation_history is None:
+        conversation_history = []
     response = openai.ChatCompletion.create(
         model=model,
-        messages=[
-            {'role': 'user', 'content': prompt}
-        ],
+        messages=[{'role': 'system', 'content': system_prompt}] +
+        conversation_history +
+        [{'role': 'user', 'content': prompt}],
         temperature=temperature,
         stream=True
     )
-
     for chunk in response:
         chunk_message = chunk['choices'][0]['delta']
-        yield chunk_message.get('content', '')
+        text = chunk_message.get('content', '')
+        yield text
 
 
 def get_full_response(prompt, print_stream=True):
     collected_messages = []
-
     for chunk in stream_chat_completion(prompt):
         collected_messages.append(chunk)
         if print_stream:
             print(chunk, end='')
-
     full_reply_content = ''.join(collected_messages)
     return full_reply_content
 
 
+# Demo
 if __name__ == '__main__':
     test_prompt = "write a story about a cat that floods the house accidentally while its owners are on vacation"
-    full_response = get_full_response(test_prompt)
-    print(f"Full conversation received: {full_response}")
+    print("Prompt:", test_prompt)
+    # stream the response
+    print("Streaming the response:")
+    get_full_response(test_prompt, print_stream=True)
+    test_prompt_2 = "write a story about a horse that floods the house accidentally while its owners are on vacation"
+    print("Prompt:", test_prompt_2)
+    # wait for the response
+    print("Waiting for the full response:")
+    full_response = get_full_response(test_prompt_2, print_stream=False)
+    print(full_response)
