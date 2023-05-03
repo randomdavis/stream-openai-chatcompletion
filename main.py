b92810b2895daa35082e5d6872c26e52ac6e4100
Random Davis <random.davis@gmail.com>
2023-05-02 20:41:47 -0700

    update main.py

--- main.py
+++ main.py
@@ -5,16 +5,46 @@ with open('apikey.txt', 'r') as f:
 
 openai.api_key = API_KEY
 
+TOKEN_LIMIT = 4097
+CHARS_PER_TOKEN = 4  # approximate
+
+conversation_history = []
+
+
+def count_tokens(messages):
+    counter = 0.0
+    for message in messages:
+        counter += len(message) / CHARS_PER_TOKEN
+    return int(round(counter))
+
 
 def stream_chat_completion(prompt: str, model: str = 'gpt-3.5-turbo', temperature=0.8,
-                           system_prompt: str = "You are a helpful AI assistant.", conversation_history=None):
-    if conversation_history is None:
-        conversation_history = []
+                           system_prompt: str = "You are a helpful AI assistant."):
+    global conversation_history
+
+    messages = [{'role': 'system', 'content': system_prompt}] + \
+        conversation_history + \
+        [{'role': 'user', 'content': prompt}]
+
+    if count_tokens(messages) >= TOKEN_LIMIT:
+        # Summarize the conversation
+        sum_messages = conversation_history + \
+            [{'role': 'user', 'content': 'Summarize the entire above conversation'}]
+        if count_tokens(sum_messages) < TOKEN_LIMIT:
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=conversation_history,
+                temperature=temperature,
+            )
+            conversation_history = [{'role': 'assistant', 'content': response.choices[0].message.content}]
+        else:
+            # as a last resort
+            while count_tokens(messages) >= TOKEN_LIMIT:
+                messages = messages[1:]
+
     response = openai.ChatCompletion.create(
         model=model,
-        messages=[{'role': 'system', 'content': system_prompt}] +
-        conversation_history +
-        [{'role': 'user', 'content': prompt}],
+        messages=messages,
         temperature=temperature,
         stream=True
     )
@@ -36,13 +66,13 @@ def get_full_response(prompt, print_stream=True):
 
 # Demo
 if __name__ == '__main__':
-    test_prompt = "write a story about a cat that floods the house accidentally while its owners are on vacation"
+    test_prompt = "write a story about a cat that floods the house accidentally while its owners are on vacation\n"
     print("Prompt:", test_prompt)
     # stream the response
     print("Streaming the response:")
     get_full_response(test_prompt, print_stream=True)
-    test_prompt_2 = "write a story about a horse that floods the house accidentally while its owners are on vacation"
-    print("Prompt:", test_prompt_2)
+    test_prompt_2 = "write a story about a horse that floods the house accidentally while its owners are on vacation\n"
+    print("\n\nPrompt:", test_prompt_2)
     # wait for the response
     print("Waiting for the full response:")
     full_response = get_full_response(test_prompt_2, print_stream=False)
